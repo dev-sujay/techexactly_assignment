acd510fa14e85bae1b9b26a3c9995f4b518b7a5d
sujay <sujaypaul531@gmail.com>
2024-09-28 01:28:14 +0530

    added fetched application data from api

--- components/Applications.jsx
+++ components/Applications.jsx
@@ -1,51 +1,69 @@
 import React, { useEffect } from 'react'
-import { View, Text, StyleSheet, ScrollView, Switch, TextInput, Image } from 'react-native'
-import AntDesign from '@expo/vector-icons/AntDesign';
+import { View, Text, StyleSheet, ScrollView, Switch, TextInput, Image, ActivityIndicator, RefreshControl } from 'react-native'
 import SearchBar from './SearchBar';
 
 const Applications = () => {
-
-    const applications = [
-        { name: 'Assistant', isEnabled: false, icon: require('../assets/googleasst.png') },
-        { name: 'Calculator', isEnabled: false, icon: require('../assets/calc1.png') },
-        { name: 'Calculator', isEnabled: false, icon: require('../assets/calc2.png') },
-        { name: 'Calendar', isEnabled: false, icon: require('../assets/calendar.png') },
-        { name: 'Chrome', isEnabled: false, icon: require('../assets/chrome.webp') },
-    ]
-    const [filteredApplications, setFilteredApplications] = React.useState(applications)
-
+    const [applications, setApplications] = React.useState([])
     const [searchText, setSearchText] = React.useState('')
+    const [loading, setLoading] = React.useState(true)
+    const [refreshing, setRefreshing] = React.useState(false)
+
+    const getApplications = async () => {
+        setLoading(true)
+        try {
+            const res = await fetch('https://navkiraninfotech.com/g-mee-api/api/v1/apps/list?kid_id=378', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+            const data = await res.json()
+            if (data.success) {
+                setApplications(data.data?.app_list?.map(app => ({ id: app.app_id, name: app?.app_name, isEnabled: app.status?.toLowerCase() === "active", icon: app?.app_icon })))
+            } else {
+                throw new Error(data.message)
+            }
+        } catch (error) {
+            console.error(error, 'error ==> getApplications');
+        } finally {
+            setLoading(false)
+        }
+    }
 
     useEffect(() => {
-        setFilteredApplications(applications.filter(app => app.name.toLowerCase().includes(searchText.toLowerCase())))
-    }, [searchText])
+        getApplications()
+    }, [])
+
 
     return (
         <View>
             <SearchBar value={searchText} onChangeText={setSearchText} />
-            <ScrollView style={styles.applicationsContainer}>
+            <ScrollView style={styles.applicationsContainer} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={getApplications} />}>
                 {
-                    filteredApplications.map((application, index) => (
-                        <View key={index} style={styles.applicationRow}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
-                                <Image source={application.icon} style={{ width: 40, height: 40, marginRight: 10 }} />
-                                <Text style={styles.applicationTitle}>{application.name}</Text>
+                    loading ?
+                        <ActivityIndicator size="large" color="#4abd77" />
+                        :
+                        applications.filter(app => app.name.toLowerCase().includes(searchText.toLowerCase())).map((application, index) => (
+                            <View key={index} style={styles.applicationRow}>
+                                <View style={styles.appInfo}>
+                                    <Image source={{ uri: application.icon }} style={{ width: 40, height: 40, marginRight: 10 }} />
+                                    <Text style={styles.applicationTitle}>{application.name}</Text>
+                                </View>
+                                <Switch
+                                    style={styles.switch}
+                                    thumbColor={application.isEnabled ? '#4abd77' : 'lightgray'}
+                                    value={application.isEnabled}
+                                    onValueChange={(value) => {
+                                        setApplications(prev => prev.map((app, i) => {
+                                            if (index === i) {
+                                                return { ...app, isEnabled: value }
+                                            }
+                                            return app
+                                        }))
+                                    }}
+                                />
                             </View>
-                            <Switch
-                                style={{ transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }] }}
-                                thumbColor={application.isEnabled ? '#4abd77' : 'lightgray'}
-                                value={application.isEnabled}
-                                onValueChange={(value) => {
-                                    setFilteredApplications(applications.map((app, i) => {
-                                        if (index === i) {
-                                            return { ...app, isEnabled: value }
-                                        }
-                                        return app
-                                    }))
-                                }}
-                            />
-                        </View>
-                    ))
+                        ))
                 }
             </ScrollView>
         </View>
@@ -69,9 +87,19 @@ const styles = StyleSheet.create({
         padding: 10,
         marginVertical: 5,
     },
+    appInfo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 5,
+        flex: 3
+    },
     applicationTitle: {
         fontSize: 20,
     },
+    switch: {
+        flex: 1,
+        transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }] 
+    }
 });
 
 export default Applications
